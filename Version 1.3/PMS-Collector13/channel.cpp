3762918e8dbcda41576bf40491d57053c0cd973a
Laveen <laveen2512@gmail.com>
2019-10-24 19:49:50 +0100

    Version 1.3 added (Code for Arduino)

---
+++ Version 1.3/PMS-Collector13/channel.cpp
@@ -0,0 +1,56 @@
+//File used to define the methods for the channel class
+
+#include <Arduino.h>
+
+#include "channel.h"
+
+// function definitions for the methods in the Channel class
+
+// Constructor method:
+// Args:
+
+Channel::Channel(void) {
+  // set attributes to default values
+  samplesToTake = 20;
+  sampleInterval = 250; // msec
+  collectMode = MODE_CAL;
+}
+
+// return the current value of the sample interval
+unsigned long Channel::getSampleInterval(void) {
+  return (sampleInterval);
+}
+
+// set the current value of the sample interval
+void Channel::setSampleInterval(int value) {
+  sampleInterval = value;
+}
+
+//Sets the number of samples to be taken
+void Channel::setSamplesToTake(int value) {
+  samplesToTake = value;
+}
+
+int Channel::getSamplesToTake(void) {
+  return (samplesToTake);
+}
+
+void Channel::setUnits(char *units) {
+  strcpy(&unitStr[0], units);
+}
+
+char *Channel::getUnits(void) {
+  return (unitStr);
+}
+
+void Channel::setMode(int value){
+  collectMode = value;
+}
+
+int Channel::getMode(void){
+  return(collectMode);
+}
+
+void Channel::getAll(void) {
+  Serial.print("DBG SampleInterval = "); Serial.println(sampleInterval);
+}
---
+++ Version 1.3/PMS-Collector13/channel.h
@@ -0,0 +1,41 @@
+//Header file used to define the class (in this case called channel)
+
+#ifndef HEADER_CHANNEL
+  #define HEADER_CHANNEL
+
+const int MODE_CAL = 0;
+const int MODE_RAW = 1;
+  
+// Class for a Channel object
+
+//Class consists of attributes and methods
+
+
+class Channel
+{  
+  // attributes
+  private: //Private variables --> Not visible outside the class
+    unsigned long sampleInterval; // time between samples (ms)
+    int samplesToTake; //Holds the number of samples to be taken in a single measurement 
+    char unitStr[4]; //Holds the units in a character array
+    int collectMode; //Mode for collecting data
+  // public:
+  // define any public attributes here
+   
+  // methods
+  public:
+    Channel(void); //Constructor method --> Is called when objects are created within the class
+    void setSampleInterval(int value);
+    unsigned long getSampleInterval(void); 
+    void setSamplesToTake(int value);
+    int getSamplesToTake(void);
+    void setUnits(char *units);
+    char *getUnits(void);
+    void setMode(int value);
+    int getMode(void);
+    void getAll(void);
+  //private:
+    // define any private methods here 
+};
+
+#endif
